d25f431ece8c83fc6092aae872c6fb545717b861
mjt <15137253025@163.com>
2018-08-27 19:28:12 +0800

    first commit

---
+++ src/main/java/TestFirst.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018/8/27.
+ */
+public class TestFirst {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaa");
+    }
+}
